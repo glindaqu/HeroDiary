d29d52b3f0f314f6de31568ccb0b5252dda33525
glindaqu <q5481996@gmail.com>
2024-04-12 04:08:46 +0700

    receiver manager added

---
+++ app/src/main/java/com/example/herodiary/notify/Notify.kt
@@ -0,0 +1,36 @@
+package com.example.herodiary.notify
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Context.ALARM_SERVICE
+import android.content.Intent
+import java.util.Calendar
+import java.util.GregorianCalendar
+
+
+fun schedulePushNotifications(context: Context) {
+    val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
+    val alarmPendingIntent by lazy {
+        val intent = Intent(context, AlarmReceiver::class.java)
+        PendingIntent.getBroadcast(context, 0, intent, 0)
+    }
+    val HOUR_TO_SHOW_PUSH = 22
+    val calendar = GregorianCalendar.getInstance().apply {
+        if (get(Calendar.HOUR_OF_DAY) >= HOUR_TO_SHOW_PUSH) {
+            add(Calendar.DAY_OF_MONTH, 1)
+        }
+
+        set(Calendar.HOUR_OF_DAY, HOUR_TO_SHOW_PUSH)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
+
+    alarmManager.setRepeating(
+        AlarmManager.RTC_WAKEUP,
+        calendar.timeInMillis,
+        AlarmManager.INTERVAL_DAY,
+        alarmPendingIntent
+    )
+}
