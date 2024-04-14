0620b8668dd909c2b7259acfe2b4ee71a34d5cc4
Isupov Vladislav <q5481996@gmail.com>
2024-04-14 14:50:55 +0700

    Update ITaskViewModel.kt

--- app/src/main/java/com/example/herodiary/viewModels/api/ITaskViewModel.kt
+++ app/src/main/java/com/example/herodiary/viewModels/api/ITaskViewModel.kt
@@ -3,8 +3,13 @@ package com.example.herodiary.viewModels.api
 import com.example.herodiary.database.room.models.TaskRoomModel
 import kotlinx.coroutines.flow.Flow
 
+// интерфейс класса TaskViewModel
 interface ITaskViewModel {
+    // добавление задачи, принимает созданный экземпляр соответствующего класса
     fun add(task: TaskRoomModel)
+    // асинхронная функция получаения id пользователя по его почте
     suspend fun getCreatorId(email: String): Int?
+    // функция получения всех связанных с пользователем (возвращает Flow, который позволяет добиться
+    // нужной степени реактивности интерфейса пользователя)
     fun getAllByCreatorEmail(email: String): Flow<List<TaskRoomModel>>
 }
